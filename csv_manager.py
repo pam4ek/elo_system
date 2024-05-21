fd07f80fc4b82a1a53df61fc45b45417599d0c88
pam4ek <thepam4ek@gmail.com>
2024-05-21 16:20:09 +0300

    rewrite sync code

--- csv_manager.py
+++ csv_manager.py
@@ -2,10 +2,9 @@ import pandas as pd
 import os
 
 def save_data(players, matches, db_file):
-    if not os.path.isdir('data'):
-        os.mkdir('data')
-    with pd.ExcelWriter('./data/' + db_file, engine='xlsxwriter') as writer:
+    directory = os.path.dirname(db_file)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    with pd.ExcelWriter(db_file, engine='xlsxwriter') as writer:
         players.to_excel(writer, sheet_name='Players', index=False)
         matches.to_excel(writer, sheet_name='Games', index=False)
-
-    # google_sheet_handler.upload_db()
